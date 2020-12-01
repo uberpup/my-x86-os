b9b02a6b4f1f94334c2dbf8442ab469ea80774ba
Vladimir <to4ilin.v@yandex.ru>
2020-12-02 01:28:43 +0300

    print! macro tests added

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,10 @@ pub trait Testable {
 impl<T> Testable for T where T : Fn(), {
     fn run(&self) {
         serial_print!("{}...\t", core::any::type_name::<T>());
-        print!("{}... ", core::any::type_name::<T>());
+        // print!("{}... ", core::any::type_name::<T>());
         self();
         serial_println!("[ok]");
-        println!("[ok!]");
+        // println!("[ok!]");
     }
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -165,3 +165,28 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+// Usage not causing panic
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+// Capable of printing and shifting lines
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+// Verifying the actual appearance of printed lines
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
