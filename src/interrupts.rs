1f337ef9d9ca74da12c5693b81d7a20ce067ad02
Vladimir <to4ilin.v@yandex.ru>
2020-12-05 17:37:10 +0300

    IDT and PIC Timer handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,15 +1,26 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
+use pic8259_simple::ChainedPics;
+use spin;
 
 use crate::println;
+use crate::print;
 //use crate::gdt;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+// unsafe cause wrong offsets can cause undefined behaviour
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
-        idt
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        return idt
     };
 }
 
@@ -24,6 +35,26 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFra
 extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame,
                                                _error_code: u64) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
+    print!(".");
+    unsafe { PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8()); }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        return self as u8;
+    }
+    fn as_usize(self) -> usize {
+        return usize::from(self.as_u8());
+    }
 }
 
 #[test_case]
--- src/lib.rs
+++ src/lib.rs
@@ -78,4 +78,6 @@ fn panic(info: &PanicInfo) -> ! {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize(); }
+    x86_64::instructions::interrupts::enable();
 }
